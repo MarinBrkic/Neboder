be926d3ecdda713715a925807474a1549c14b80e
MarinBrkic <152974525+MarinBrkic@users.noreply.github.com>
2024-03-15 19:52:29 +0100

    Add files via upload

---
+++ Zgrada/Zgrada/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Zgrada
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.unosBrojaKatova = new System.Windows.Forms.TextBox();
+            this.unosButton = new System.Windows.Forms.Button();
+            this.ispisVisine = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // unosBrojaKatova
+            // 
+            this.unosBrojaKatova.Location = new System.Drawing.Point(62, 59);
+            this.unosBrojaKatova.Name = "unosBrojaKatova";
+            this.unosBrojaKatova.Size = new System.Drawing.Size(167, 22);
+            this.unosBrojaKatova.TabIndex = 0;
+            this.unosBrojaKatova.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // unosButton
+            // 
+            this.unosButton.Location = new System.Drawing.Point(104, 106);
+            this.unosButton.Name = "unosButton";
+            this.unosButton.Size = new System.Drawing.Size(86, 26);
+            this.unosButton.TabIndex = 1;
+            this.unosButton.Text = "unos";
+            this.unosButton.UseVisualStyleBackColor = true;
+            this.unosButton.Click += new System.EventHandler(this.unosButton_Click);
+            // 
+            // ispisVisine
+            // 
+            this.ispisVisine.Location = new System.Drawing.Point(62, 166);
+            this.ispisVisine.Name = "ispisVisine";
+            this.ispisVisine.ReadOnly = true;
+            this.ispisVisine.Size = new System.Drawing.Size(167, 22);
+            this.ispisVisine.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Unos broja katova:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 147);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ispis visine:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 243);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ispisVisine);
+            this.Controls.Add(this.unosButton);
+            this.Controls.Add(this.unosBrojaKatova);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox unosBrojaKatova;
+        private System.Windows.Forms.Button unosButton;
+        private System.Windows.Forms.TextBox ispisVisine;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Zgrada/Zgrada/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+
+namespace Zgrada
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private class Neboder
+        {
+            public int visina, brojKatova;
+
+            public void setVisina(int visina)
+            {
+                this.visina = visina;
+            }
+            public int getVisina()
+            {
+                return this.visina;
+            }
+            public void setBrojKatova(int brojKatova)
+            {
+                this.brojKatova = brojKatova;
+            }
+            public int getBrojKatova()
+            {
+                return this.brojKatova;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            { 
+
+            }
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message, "Greška unosa", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void unosButton_Click(object sender, EventArgs e)
+        {
+            Neboder neboder = new Neboder();
+            int brojKatova;
+            int visina = 0;
+            try
+            {
+                brojKatova = Convert.ToInt32(unosBrojaKatova.Text);
+                neboder.setVisina(visina);
+                neboder.setBrojKatova(brojKatova);
+                if (brojKatova <= 0)
+                {
+                    MessageBox.Show("Broj katova ne može iznositi 0 " +
+                        "ili biti negativan broj!", "KRIVI UNOS!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+
+                }
+                else
+                {
+                    visina = brojKatova * 3;
+                    ispisVisine.Text = visina.ToString();
+                }
+            }
+
+            catch (Exception greska)
+            {
+                MessageBox.Show(greska.Message, "Greška unosa", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
